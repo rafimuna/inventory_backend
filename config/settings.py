e786312356d40f165e639ed8f49636d1b216be88
rafimuna <rafimuna77@gmail.com>
2025-09-18 12:08:01 +0600

    Update ALLOWED_HOSTS for Render and gunicorn port

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = ["inventory-backend-latest-ykaw.onrender.com"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
